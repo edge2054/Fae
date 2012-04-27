2deafe4844f5efe13bb50095d45092159cbe3a49
Eric Wykoff <ericwykoff@yahoo.com>
2012-04-27 16:49:33 -0500

    basic character sheet

---
+++ dialogs/CharacterSheet.lua
@@ -0,0 +1,75 @@
+-- Fae
+-- Copyright (C) 2012 Eric Wykoff
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Eric Wykoff "edge2054"
+-- edge2054@gmail.com
+
+require "engine.class"
+
+local Dialog = require "engine.ui.Dialog"
+local Talents = require "engine.interface.ActorTalents"
+local SurfaceZone = require "engine.ui.SurfaceZone"
+local Stats = require "engine.interface.ActorStats"
+local Textzone = require "engine.ui.Textzone"
+
+module(..., package.seeall, class.inherit(Dialog))
+
+function _M:init(actor)
+    self.actor = actor
+    
+    self.font = core.display.newFont("/data/font/VeraMono.ttf", 12)
+    Dialog.init(self, "Character Sheet: "..self.actor.name, math.max(game.w * 0.7, 950), 500, nil, nil, font)
+    
+    self.c_desc = SurfaceZone.new{width=self.iw, height=self.ih,alpha=0}
+
+    self:loadUI{
+        {left=0, top=0, ui=self.c_desc},
+    }
+    
+    self:setupUI()
+    
+    self:drawDialog()
+    
+    self.key:addBind("EXIT", function() cs_player_dup = game.player:clone() game:unregisterDialog(self) end)
+end
+
+function _M:drawDialog()
+    local player = self.actor
+    local s = self.c_desc.s
+
+    s:erase(0,0,0,0)
+
+    local h = 0
+    local w = 0
+
+    h = 0
+    w = 0
+    s:drawStringBlended(self.font, "Name : "..(player.name or "Unnamed"), w, h, 255, 255, 255, true) h = h + self.font_h
+    s:drawStringBlended(self.font, "Role : "..(player.descriptor.role or player.type:capitalize()), w, h, 255, 255, 255, true) h = h + self.font_h
+        
+    h = h + self.font_h -- Adds an empty row
+    
+    h = 0
+    w = self.w * 0.25 
+    -- start on second column
+        
+    s:drawStringBlended(self.font, "STR : "..(player:getStr()), w, h, 0, 255, 255, true) h = h + self.font_h
+    s:drawStringBlended(self.font, "DEX : "..(player:getDex()), w, h, 255, 0, 255, true) h = h + self.font_h
+    s:drawStringBlended(self.font, "CON : "..(player:getCon()), w, h, 255, 255, 0, true) h = h + self.font_h
+    
+    self.c_desc:generate()
+    self.changed = false
+end
